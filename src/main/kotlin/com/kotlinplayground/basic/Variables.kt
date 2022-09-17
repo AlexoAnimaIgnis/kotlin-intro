aabd3512618505681fe8af3d400d7b9417ff10f2
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-09-17 16:16:09 +0800

    Kotlin 02: val and var

---
+++ src/main/kotlin/com/kotlinplayground/basic/Variables.kt
@@ -0,0 +1,12 @@
+package com.kotlinplayground.basic
+
+fun main() {
+    val name : String = "Alexo"
+    println(name)
+
+    // name = "Raphaela" -- not allowed as val is immutable
+    var age : Int = 18
+    println(age)
+    age = 19
+    println(age)
+}
