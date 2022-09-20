fb4b84a37d57be9d6a200a47f0223f7d58203002
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-09-20 10:54:49 +0800

    Inheritance overriding functions and variables

--- src/main/kotlin/com/kotlinplayground/oop/Inheritance.kt
+++ src/main/kotlin/com/kotlinplayground/oop/Inheritance.kt
@@ -1,14 +1,28 @@
 package com.kotlinplayground.oop
 
-open class User(val name: String)
+open class User(val name: String) {
+    open var isLoggedIn : Boolean = false
+    open fun login() {
+        println("Inside User Login")
+    }
+}
 
-class Student(name: String): User(name)
+class Student(name: String): User(name) {
+    override var isLoggedIn : Boolean = true
+    override fun login() {
+        println("Inside Student login")
+        super.login()
+    }
+}
 
 class Instructor(name: String): User(name)
 
 fun main() {
     val student = Student("alexo")
-    val instructor = Instructor("alexo")
     println("Student is: ${student.name}")
+    student.login()
+    println("Student LoggedIn value is: ${student.isLoggedIn}")
+    val instructor = Instructor("alexo")
     println("Instructor is: ${instructor.name}")
+    instructor.login()
 }
